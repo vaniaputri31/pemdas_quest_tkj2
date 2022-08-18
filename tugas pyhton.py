aa85c83aa6e7143ac16846b435455b69190e8344
vaniaputri31 <111480392+vaniaputri31@users.noreply.github.com>
2022-08-17 23:58:31 -0700

    Add files via upload

---
+++ tugas pyhton.py
@@ -0,0 +1,8 @@
+nama_depan="vania"
+nama_tengah="avila"
+nama_belakang="putri"
+nama_lengkap="vania avila putri"
+print("nama depan anda adalah",nama_depan)
+print("nama tengah saya adalah",nama_tengah)
+print("nama belakang saya adalah",nama_belakang)
+print("nama lengkap saya adalah",nama_depan,nama_tengah,nama_belakang)

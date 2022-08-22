d248cca0ec2867a0605d4920e29b6c8e9a02e7bd
vaniaputri31 <111480392+vaniaputri31@users.noreply.github.com>
2022-08-21 22:13:35 -0700

    Add files via upload

---
+++ pyhton4.py
@@ -0,0 +1,16 @@
+print("hallo")
+print("perkenalkan")
+nama_saya_adalah=input("nama saya adalah")
+nama_lengkap=input("nama lengkap saya adalah")
+
+a = int(input("masukan nilai A: "))
+b = float(input("masukan nilai B: "))
+c = a + b
+
+print("A =", a)
+print("B =", b)
+print(a,"+",b,"=", c)
+
+x = int(input("masukan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius =",x, "fahrenhait")

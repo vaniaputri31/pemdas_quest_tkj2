a08c47b31ce69b379ccb347094f4226f3d93cc75
vaniaputri31 <111480392+vaniaputri31@users.noreply.github.com>
2023-02-12 18:52:00 -0800

    Add files via upload

---
+++ vania_x_tkj2/latihan_fungsi.py
@@ -0,0 +1,42 @@
+'''fungsi pada pyhton didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("Assalamu'alaikum !! Akhi Ukhty")
+
+salam() 
+salam()
+salam()
+
+'''buatlah sebuah fungsi versi kalian'''
+
+def bertanya():
+    print("kemana saja?")
+
+bertanya()
+bertanya()
+bertanya()
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("Halo perkenalkan nama saya", nama)
+
+say_hi("putri")
+say_hi("vina")
+
+'''Buatlah fungsi dengan 3 parameter'''
+
+def perkenalan(nama, umur, sekola):
+    # Metode
+    print("nama saya:", nama)
+    print("Umur saya:", umur, 'tahun')
+    print("sekola saya:", sekola)
+
+perkenalan("riyan", 17, "Assalam")
+
+print("\n\n============= BATAS =============\n\n")
+
+perkenalan("reza", 18, "Assalam")
